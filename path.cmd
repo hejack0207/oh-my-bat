92078e24081c80a89ddfc964212c10487f03c37e
hejack <hejack0207@sina.com>
2021-02-18 18:16:55 +0800

    fix

--- path.cmd
+++ path.cmd
@@ -19,21 +19,32 @@ if "x%~1" == "x/?" (
   call :printhelp "%~nx0"
   exit /b %ERRORLEVEL%
 )
+if "x%~1" == "xappend" (
+  call :append_path "%2"
+  exit /b %ERRORLEVEL%
+)
+if "x%~1" == "xquery" (
+  call :show_path
+  exit /b %ERRORLEVEL%
+)
 exit /b 0
 
 call :checkparams
-@echo "%1"
-call :appendpath "%1"
 goto :EOF
 
-:appendpath
-set value=%path%;%1
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Sessions Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%value%" /f
+:append_path
+@echo %1
+REM  set value=%path%;%1
+REM  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%value%" /f
+exit /b 0
+
+:show_path
+reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path
 exit /b 0
 
 :printhelp
 echo Usage:
-echo     %~1 [options] path...
+echo     %~1 query or append path...
 echo.
 echo Options:
 echo     -help ^| --help ^| -? ^| /?         Display this help and exit
