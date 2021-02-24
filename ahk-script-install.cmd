aa57f8c91fd51832e0071e7925352a1dfc26def7
hejack <hejack0207@sina.com>
2021-02-24 13:21:33 +0800

    fix

--- ahk-script-install.cmd
+++ ahk-script-install.cmd
@@ -4,8 +4,9 @@ setlocal EnableDelayedExpansion
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 @echo "startup: %startup%"
 
+copy ahk\CapsLocks.ahk c:\CapsLocks.ahk"
 copy ahk\default.ahk "%startup%\default.ahk"
+
 REM  if exist "%startup%\clipjump" mkdir "%startup%\clipjump"
 REM  xcopy /y /s ahk\clipjump "%startup%\clipjump"
-
 REM AutoHotkey "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\default.ahk"
