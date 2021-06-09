9503ece164a120476c68f9ceb7f83139b4c44898
hejack <hejack0207@sina.com>
2021-06-09 18:54:58 +0800

    fix

--- ahk-script-install.cmd
+++ ahk-script-install.cmd
@@ -4,14 +4,16 @@ setlocal EnableDelayedExpansion
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 @echo "startup: %startup%"
 
-mkdir c:\ahk
-copy ahk\CapsLocks.ahk c:\ahk\CapsLocks.ahk"
-copy ahk\customized.ahk c:\ahk\customized.ahk"
-copy ahk\vim_mouse_2.ahk c:\ahk\vim_mouse_2.ahk"
-mkdir c:\ahk\bug.n
-xcopy /y /s ahk\bug.n c:\ahk\bug.n
+REM  mkdir c:\ahk
+REM  copy ahk\CapsLocks.ahk c:\ahk\CapsLocks.ahk"
+REM  copy ahk\customized.ahk c:\ahk\customized.ahk"
+REM  copy ahk\vim_mouse_2.ahk c:\ahk\vim_mouse_2.ahk"
+REM  mkdir c:\ahk\bug.n
+REM  xcopy /y /s ahk\bug.n c:\ahk\bug.n
 
-copy ahk\startup.ahk "%startup%\startup.ahk"
+if not exist "%startup%\startup.ahk" (
+	copy ahk\startup.ahk "%startup%\startup.ahk"
+)
 
 REM  if exist "%startup%\clipjump" mkdir "%startup%\clipjump"
 REM  xcopy /y /s ahk\clipjump "%startup%\clipjump"
