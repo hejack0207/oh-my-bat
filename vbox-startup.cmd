ea762ee1f8e9851f52c31f6dcb7cdf22cca925e8
hejack <hejack0207@sina.com>
2022-05-03 09:47:10 +0800

    fix

--- vbox-startup.cmd
+++ vbox-startup.cmd
@@ -1,5 +1,27 @@
 @echo off
-REM  start "vbox headless" "C:\Program Files\Oracle\VirtualBox\VBoxHeadless.exe" -s default
-REM  --type headless
-REM  start "vbox headless" "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm default
-"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm default --type headless
+rem  start "vbox headless" "C:\Program Files\Oracle\VirtualBox\VBoxHeadless.exe" -s default
+rem  --type headless
+rem  start "vbox headless" "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm default
+
+where vboxmanage 2>nul
+if errorlevel 1 (
+	set vb="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" 
+) else (
+	set vb=vboxmanage
+)
+
+set vm=%1
+
+%vb% list vms | findstr -i %vm% >nul
+if errorlevel 1 (
+	echo "%vm% not found"
+	exit /b 1
+)
+
+%vb% list runningvms | findstr -i %vm% >nul
+if errorlevel 0 (
+	echo "%vm% already started"
+	exit /b 1
+)
+
+%vb% startvm %vm% --type headless
