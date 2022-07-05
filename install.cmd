21cf18689f56e5d77eb5aa5269a3558e3c33574c
hejack <hejack0207@sina.com>
2022-07-05 21:45:26 +0800

    fix

--- install.cmd
+++ install.cmd
@@ -1,8 +1,8 @@
 REM  auto startup
 
-call startup.cmd query | findstr -i putty 2> nul
-if ERRORLEVEL 1 (
-	call startup.cmd append "C:\Program Files\PuTTY\"
-)
+REM call startup.cmd query | findstr -i putty 2> nul
+REM if ERRORLEVEL 1 (
+REM 	call startup.cmd append "C:\Program Files\PuTTY\"
+REM )
 
 call ahk-script-install.cmd
--- omb-autostartup.cmd
+++ omb-autostartup.cmd
@@ -1,3 +1,3 @@
-call c:/oh-my-bat/vbox-startup.cmd fedora35
-call c:/oh-my-bat/vbox-startup.cmd euler2203
+REM call c:/oh-my-bat/vbox-startup.cmd fedora35
+REM call c:/oh-my-bat/vbox-startup.cmd euler2203
 call c:/oh-my-bat/install.cmd
